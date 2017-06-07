8c5050663f5d1b2ff0ad5288f09f6e064e79e569
Palm <tauman.exe@gmail.com>
2017-06-07 14:50:21 +0200

    Added HitPoints to the project.

---
+++ Assets/src/SceneGizmos/HitPoints.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using UnityEngine.Events;
+
+public class HitPoints : NetworkBehaviour
+{
+	[SerializeField, SyncVar(hook = "OnChangeMax")]
+	int maxHealth;
+	[SerializeField, SyncVar(hook = "OnChangeDamage")]
+	int damageTaken;
+	
+	public HealthEvent EventChangeHealth;
+	public IntEvent EventHurt;
+	public IntEvent EventHeal;
+
+	void Start()
+	{
+		EventChangeHealth.Invoke(CurrentHealth, MaxHealth);
+	}
+
+	public int CurrentHealth
+	{
+		get
+		{
+			return maxHealth - damageTaken;
+		}
+		set
+		{
+			if (maxHealth - value < 0)
+				damageTaken = 0;
+			else
+				damageTaken = maxHealth - value;
+		}
+	}
+	public float HealthPercent
+	{
+		get
+		{
+			return CurrentHealth / (float)maxHealth;
+		}
+	}
+	public int MaxHealth
+	{
+		get
+		{
+			return maxHealth;
+		}
+
+		set
+		{
+			maxHealth = value;
+		}
+	}
+
+	public void Hurt(int d)
+	{
+		damageTaken += d;
+		EventHurt.Invoke(d);
+	}
+	
+	public void Heal(int d)
+	{
+		CurrentHealth += d;
+		EventHeal.Invoke(d);
+	}
+
+	void OnChangeDamage(int damageTaken)
+	{
+		EventChangeHealth.Invoke(maxHealth - damageTaken, maxHealth);
+	}
+
+	void OnChangeMax(int max)
+	{
+		EventChangeHealth.Invoke(max - damageTaken, max);
+	}
+
+	[System.Serializable]
+	public class HealthEvent : UnityEvent<int, int> { }
+	[System.Serializable]
+	public class IntEvent : UnityEvent<int> { }
+}
