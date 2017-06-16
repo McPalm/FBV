939767e122622bb8c65adf4e256cf55dbb84757c
Palm <tauman.exe@gmail.com>
2017-06-16 14:12:31 +0200

    No moving into occupied space

--- Assets/src/Rules/MovementRules.cs
+++ Assets/src/Rules/MovementRules.cs
@@ -9,8 +9,31 @@ public class MovementRules : MonoBehaviour
 	{
 		IntVector2 orig = GetComponent<Mobile>().Location;
 
-		HashSet<IntVector2> s = new HashSet<IntVector2>(IntVector2Utility.GetRect(orig - new IntVector2(5, 5), orig + new IntVector2(5, 5)));
+		HashSet<IntVector2> s = new HashSet<IntVector2>();
+
+		foreach(IntVector2 iv2 in IntVector2Utility.GetRect(orig - new IntVector2(5, 5), orig + new IntVector2(5, 5)))
+		{
+			MapTerrain t = Obstructions.Instance.TerrainAt(iv2);
+			MapObject o = Obstructions.Instance.MobileAt(iv2);
+			if (o)
+			{
+
+				if (iv2 == orig) s.Add(iv2);
+				// no bueno
+			}
+			else if (t)
+			{
+				if (!t.wall & !t.water)
+					s.Add(iv2);
+			}
+			else
+				s.Add(iv2);
+		}
+
 		
+		
+
+
 		return s;
 	}
 }
