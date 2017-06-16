c32b036b0b7e9b17562158dbadc0dcf0bdbcd18c
Palm <tauman.exe@gmail.com>
2017-06-16 13:40:20 +0200

    A limiter for where you can move units.

--- Assets/src/Controller/UnitController.cs
+++ Assets/src/Controller/UnitController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 
 /// <summary>
@@ -17,11 +18,12 @@ public class UnitController : AController
 	GameObject pickupOrigin;
 	System.Action State;
 	Draggable heldPiece;
-
+	HashSet<IntVector2> moveTiles;
 
 	public GameObjectEvent EventSelect = new GameObjectEvent();
 	public UnityEvent EventStateNeutral = new UnityEvent();
 	public UnityEvent EventStatePickup = new UnityEvent();
+	public IV2ArrayEvent EventMoveTiles = new IV2ArrayEvent();
 
 	void Awake()
 	{
@@ -55,6 +57,15 @@ public class UnitController : AController
 					pickupOrigin.transform.position = heldPiece.transform.position;
 					dropShadow.position = MousePosition + new Vector3(0f, 0f, 10f);
 					EventSelect.Invoke(heldPiece.gameObject);
+					if (d.GetComponent<MovementRules>())
+					{
+						moveTiles = d.GetComponent<MovementRules>().ReachableTiles();
+						IntVector2[] arr = new IntVector2[moveTiles.Count];
+						moveTiles.CopyTo(arr);
+						EventMoveTiles.Invoke(arr);
+					}
+					else
+						moveTiles = null;
 				}
 			}
 		}
@@ -93,6 +104,8 @@ public class UnitController : AController
 
 	public bool CanMoveTo(Mobile user, IntVector2 targetLocation)
 	{
+		if (moveTiles != null)
+			return moveTiles.Contains(targetLocation);
 		return true;
 		/*
 		IntVector2 orig = IntVector2.RoundFrom(user.GetComponent<Mobile>().location);
@@ -107,4 +120,6 @@ public class UnitController : AController
 
 	[System.Serializable]
 	public class GameObjectEvent : UnityEvent<GameObject> { }
+	[System.Serializable]
+	public class IV2ArrayEvent : UnityEvent<IntVector2[]> { }
 }
---
+++ Assets/src/Rules/MovementRules.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Mobile))]
+public class MovementRules : MonoBehaviour
+{
+	public HashSet<IntVector2> ReachableTiles()
+	{
+		IntVector2 orig = GetComponent<Mobile>().Location;
+
+		HashSet<IntVector2> s = new HashSet<IntVector2>(IntVector2Utility.GetRect(orig - new IntVector2(5, 5), orig + new IntVector2(5, 5)));
+		
+		return s;
+	}
+}
