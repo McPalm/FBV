a116d23fb16a19a622f3060c85797a0ecc7f91a2
Palm <tauman.exe@gmail.com>
2017-06-25 11:49:22 +0200

    A UI class that handles a list of buttons

---
+++ Assets/src/UI/ButtonArray.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ButtonArray : MonoBehaviour
+{
+	[SerializeField]
+	Button[] buttons;
+	Image[] icons;
+
+	public IntEvent EventButtonPressed = new IntEvent();
+
+	// Use this for initialization
+	void Awake()
+	{
+		icons = new Image[buttons.Length];
+		for(int i = 0; i < buttons.Length; i++)
+		{
+			icons[i] = buttons[i].GetComponentsInChildren<Image>()[1];
+
+			int local = i;
+			UnityAction observer = () => EventButtonPressed.Invoke(local);
+			buttons[i].onClick.AddListener(observer);
+		}
+		
+	}
+
+	public void SetIcon(int index, Sprite icon)
+	{
+		if (index < icons.Length)
+			icons[index].sprite = icon;
+		else
+			Debug.LogError("Index " + index + " not supported in " + name + "<ButtonArray>");
+	}
+
+	[System.Serializable]
+	public class IntEvent : UnityEvent<int> { }
+
+}
