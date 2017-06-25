32e1c2c91ecb43c87b1cea91dfb50389953e9aa0
Palm <tauman.exe@gmail.com>
2017-06-25 12:24:07 +0200

    press 1 - 4 to select that ability.

---
+++ Assets/src/UI/UIGizmos/HotKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Put this on a UnityEngine.UI.Button to assign a hotkey to that button.
+/// </summary>
+
+public class HotKey : MonoBehaviour
+{
+	public string key;
+
+	void Update ()
+	{
+		if (Input.GetButtonDown(key))
+		{
+			GetComponent<Button>().onClick.Invoke();
+		}
+	}
+}
