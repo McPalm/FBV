5363e470da6bc2a3e80f8851b1fd67f252c25b10
Palm <tauman.exe@gmail.com>
2017-06-26 14:41:36 +0200

    A button to leave games in process.

---
+++ Assets/src/UI/Networking/InGameNetworkUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class InGameNetworkUI : MonoBehaviour
+{
+	public void Disconnect()
+	{
+		NetworkManager.singleton.StopServer();
+		NetworkManager.singleton.StopClient();
+	}
+}
