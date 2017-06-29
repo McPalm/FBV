736a62c249efca430bd6a622b964581b44a795ef
Palm <tauman.exe@gmail.com>
2017-06-29 13:56:23 +0200

    Mouseover to see stats of characters.
    When having a unit selected, always see their stats instead.

---
+++ Assets/src/UI/UIGizmos/MouseoverCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseoverCharacter : MonoBehaviour
+{
+	public CharacterEvent EventOnMouseOver = new CharacterEvent();
+	public UnityEvent EventLeaveMouseOver = new UnityEvent();
+
+	Character lastMouseOver;
+
+	void LateUpdate()
+	{
+		Character c = AController.ComponentUnderMouse<Character>();
+		if(c != lastMouseOver)
+		{
+			if (c)
+				EventOnMouseOver.Invoke(c);
+			else
+				EventLeaveMouseOver.Invoke();
+			lastMouseOver = c;
+		}
+	}
+
+	void OnDisable()
+	{
+		lastMouseOver = null;
+	}
+
+	[System.Serializable]
+	public class CharacterEvent : UnityEvent<Character> { }
+}
